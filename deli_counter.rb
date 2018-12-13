e0e8e16ebc4d7bffa8a5ce237e8bb3ece6776ee0
Sam Json <3115002443@mail2.gdut.edu.cn>
2018-12-13 07:14:09 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+def line deli
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently: "
+    deli.each_with_index do |name,index|
+      string << "#{index+1}. #{name} "
+    end
+    puts string.chop
+  end
+end
+
+def take_a_number deli,name
+  deli << name
+  rank = deli.index(name) + 1
+  puts "Welcome, #{name}. You are number #{rank} in line."
+end
+
+def now_serving deli
+  if deli.length == 0
+    puts "There is nobody waiting to be served!"
+  else
+    name = deli.shift
+    puts "Currently serving #{name}."
+  end
+end
