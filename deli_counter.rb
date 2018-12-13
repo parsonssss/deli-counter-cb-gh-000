bf2a67effa87db0ed2ce11368349be807bac22bb
Sam Json <3115002443@mail2.gdut.edu.cn>
2018-12-13 07:14:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+def line deli
+  if deli.length == 0
+    puts "The line is currently empty."
+  else
+    string = "The line is currently: "
+    deli.each_with_index do |name,index|
+      string << "#{index+1}. #{name} "
+    end
+    puts string
+  end
+end
